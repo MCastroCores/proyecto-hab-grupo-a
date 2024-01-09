07ea95d3af5ad67843874290fc5956b7f657bebd
Adrián Acuña Cabrera <adri.acu.95@gmail.com>
2024-01-09 15:13:12 +0100

    Crear funcionalidades nuevas

--- main.js
+++ main.js
@@ -1,55 +1,98 @@
 'use strict'
 
-const cartas = document.querySelectorAll('.card');
-
-const tablero = document.querySelector('.container');
-
-console.log(cartas);
-
-const cartasArray = Array.from(cartas);
-
-console.log(cartasArray);
-
-let j = 0;
-
-function barajar (array) {
-    for (let i = 0; i < array.length; i++){
-
-        let j = Math.floor(Math.random()* (i+1)) 
-
-        console.log(j);
-
-        [array[i], array[j]] = [array[j], array[i]]
-
-    }
-
-}
-
-barajar(cartasArray)
-
-cartasArray.forEach(carta => {
-    tablero.append(carta)
-});
-
+//* Constantes del HTML
 const cards = document.querySelectorAll(".card");
-const result = document.querySelector('.result');
+let firstCard = null;
+let numDeclicks = 0;
 
-let firstCard = null
-let numDeclicks = 0
+//* Constantes del JUEGO 
+const tablero = document.querySelector('.tablero');
+const celdas = document.querySelectorAll('.carta');
+
+//* Constantes del FINAL
+const result = document.querySelector('.result');
 let contador = 0;
 let contadorFlipped = 0;
 
-const finalizar = () => {
-  if (contadorFlipped === 8){
-    console.log('Has ganado');
-  } else {
-    console.log('no has ganado');
+// *Constantes Barajar
+
+const cartasArray = Array.from(celdas);
+let j = 0;
+
+//*Funcion Barajar 
+
+function barajar (array) {
+      for (let i = 0; i < array.length; i++){
+            let j = Math.floor(Math.random()* (i+1)) 
+        console.log(j);
+        [array[i], array[j]] = [array[j], array[i]]
+    }
+      array.forEach(carta => {
+            tablero.append(carta)
+        });
+};
+
+
+//* Función Mostrar Cartas
+
+function mostrarCartas() {
+  cards.forEach(card => {
+    card.classList.add("flipped");
+    
+  });
+  setTimeout(() => {
+    cards.forEach(card => {
+      card.classList.remove("flipped")
+    });
+  }, 6000);
+};
+
+
+
+
+  // * Función Felicitar
+  function felicitar() {
+    const congrats = document.createElement('p');
+    congrats.classList.add("felicitar");
+    congrats.textContent = "WELL DONE!";
+    tablero.append(congrats);
+    setTimeout(() => {
+      congrats.remove();
+    }, 1000);
+  };
+  
+  // *Funcion HAS GANADO
+  function finalJuego() {
+    const hasGanadoModal = document.createElement('div');    
+    hasGanadoModal.classList.add("fondo");
+    hasGanadoModal.innerHTML = `
+    <div class="finalJuego">YOU WON!</div>
+    `;
+    tablero.append(hasGanadoModal);
   }
-}
 
+  // * Funcion Fin de Juego
+  
+  const finalizar = () => {
+    if (contadorFlipped === 8){
+      finalJuego();
+    } else {
+      felicitar();
+    };
+  };
 
-const reveal = (e) => {
-  if (numDeclicks < 2) {
+  // * Boton Reset
+  
+  const btnReset = document.querySelector(".reset");
+  btnReset.addEventListener('click', () => {
+    // location.reload();
+    iniciarJuego();
+    // hasGanadoModal.classList.add();
+  });
+  
+  // * Función Juego
+  const reveal = (e) => {
+    if (numDeclicks < 2) {
     const currentCard = e.currentTarget;
      if (currentCard.classList.contains("flipped")) {
       return;
@@ -73,7 +116,7 @@ const reveal = (e) => {
         console.log(contadorFlipped);
         finalizar();
         return contadorFlipped;
-
+        
       } else {
         
         setTimeout(() => {
@@ -85,20 +128,45 @@ const reveal = (e) => {
         result.textContent = contador;
       }
     }
-        numDeclicks++
-        if (numDeclicks = 2){
-          setTimeout(() => {
-            numDeclicks = 0
-      
-          }, 1050);
+    numDeclicks++
+    if (numDeclicks = 2){
+      setTimeout(() => {
+        numDeclicks = 0
+        
+      }, 1050);
       }
     }
 };
 
-for (const card of cards) {
-  card.addEventListener("click", reveal);
+// ? AQUÍ ORDENAMOS CODIGO EN FUNCIÓN DE SUS PASOS
 
+function iniciarJuego() {
+  barajar(cartasArray);
+  const start = setTimeout(() => {
+    mostrarCartas(cards);
+    // * Constantes Cuenta Atrás
+    let cronometro = 5;
+     let intervalo = document.querySelector(".cuentaatras")
+     let seccioninicio = document.querySelector(".iniciocontador")
+    
+    //  * Función Cuenta Atrás
+     let iniciojuego = setInterval(() => {
+       intervalo.textContent = cronometro;
+       seccioninicio.classList.remove("invisible")
+       cronometro--;
+       if(cronometro === -1){
+         clearInterval(iniciojuego);
+         seccioninicio.classList.add("invisible");
+        };
+      }, 1000); 
+  }, 500);
 }
-function resetearJuego() {
-  location.reload();
+
+iniciarJuego();
+
+// * Bucle Cartas
+for (const card of cards) {
+  card.addEventListener("click", reveal);  
 }
+
+//*
